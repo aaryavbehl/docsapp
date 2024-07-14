9f01df954f270c6a51254687403c4eda5ac587d7
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-14 22:48:19 +0530

    modified:   Documents/package.json

--- Documents/src/components/Foreground.jsx
+++ Documents/src/components/Foreground.jsx
@@ -2,13 +2,32 @@ import React, { useState } from 'react'
 import Card from './card'
 
 function Foreground() {
+
+  const ref = useRef(null);
+
   const data = [
-     
+     {desc: "THIS WILL BE YOUR DESCRIPTION",
+       filesize: ".9mb", 
+       close: true, 
+       tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green"}
+      },
+      {desc: "THIS WILL BE YOUR DESCRIPTION",
+        filesize: ".9mb", 
+        close: true, 
+        tag: {isOpen: true, tagTitle: "Download Now", tagColor: "blue"}
+       },
+       {desc: "THIS WILL BE YOUR DESCRIPTION",
+        filesize: ".9mb", 
+        close: true, 
+        tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green"}
+       },
   ];
   useState()
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-        <Card/>
+    <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+        {data.map((item, index)=>(
+          <Card data={item} reference={ref}/>
+        ))}
     </div>
   )
 }
--- Documents/src/components/card.jsx
+++ Documents/src/components/card.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { FaRegWindowClose } from "react-icons/fa";
+import { motion } from "framer-motion"
 
-function Card() {
+function Card({data, reference}) {
   return(
-    <div className='relative w-60 h-72 rounded-[40px] bg-zinc-500 text-white px-8 py-10 overflow-hidden'>
+    <motion.div drag dragConstraints={red} className='relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-500 text-white px-8 py-10 overflow-hidden'>
       <FaRegFileAlt/>
-      <p className="text-sm leading-tight mt-5 font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      <p className="text-sm leading-tight mt-5 font-semibold">{data.desc}</p>
       <div className="footer absolute bottom-0 w-full left-0">
         <div className="flex items-center justify-between px-8 py-3 mb-3">
-          <h4>.4mb</h4>
-          <LuDownload size=".9rem" color="purple"/>
+          <h4>{data.filesize}</h4>
+          <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
+          {data.close ? <FaRegWindowClose/> : <LuDownload size=".9rem" color="purple"/> }
+          </span>
         </div>
-        <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-          <h3 className="text-sm font-semibold">Download Now</h3>
-        </div>
+        {
+          data.tag.isOpen && (
+            <div className={`tag w-full py-4 ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
+            <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+            </div>
+          )}        
       </div>
-    </div>
+    </motion.div>
   )
 }
 
